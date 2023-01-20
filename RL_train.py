5045f3f0956ae43e8a8fac3ec7950ce9edd3b114
Om Daxini <omdaxini@gmail.com>
2023-01-20 19:53:31 +0530

    Update and rename ddpg_RL_train.py to RL_train.py
    Changed the code and the code is running on A2C algorithm

--- RL_train.py
+++ RL_train.py
@@ -7,21 +7,21 @@ import datetime
 import stable_baselines3
 from tensorflow import keras
 from tensorflow.keras.callbacks import TensorBoard
-from stable_baselines3 import DDPG
+from stable_baselines3 import A2C
 from gym.envs.registration import registry, register, make, spec
 from stable_baselines3.common.vec_env import SubprocVecEnv
-
+  #Register the RockerLander environment
+register(
+  id='RocketLander-v0',
+  entry_point='rocket_lander:RocketLander',#gym.envs.box2d:
+  max_episode_steps=1000,
+  reward_threshold=0,
+)
 if __name__ == '__main__':
   tensorboard = TensorBoard(log_dir='logs')
-  #Register the RockerLander environment
-  register(
-    id='RocketLander-v0',
-    entry_point='rocket_lander:RocketLander',#gym.envs.box2d:
-    max_episode_steps=1000,
-    reward_threshold=0,
-  )
 
-  timestep = 20000000
+
+  timestep = 2000000
   ENV  = 'RocketLander-v0'
   timestamp = datetime.datetime.now()
   filename = "ppo2_{}_{}_{}".format(ENV,timestep,str(timestamp)[:19])
@@ -32,12 +32,12 @@ if __name__ == '__main__':
   n_cpu = 8
   env = SubprocVecEnv([make_env for i in range(n_cpu)])
 
-  config = tf.ConfigProto()
+  config = tf.compat.v1.ConfigProto()
 
-  with tf.Session(config=config):
+  with tf.compat.v1.Session(config=config):
   
-    model = DDPG("MlpPolicy", env,n_steps=1024,nminibatches=256,lam=0.95,gamma=0.99,noptepochs=3,ent_coef=0.01,learning_rate=lambda _: 1e-4,cliprange=lambda _: 0.2, tensorboard_log=path,full_tensorboard_log=True,verbose=2)
-    model.learn(total_timesteps=timestep, log_interval=1000 , callback = tensorboard)#15M timesteps and overnight run on a Macbook worked fine(still can improve).
+    model = A2C("MlpPolicy", env,verbose=2)
+    model.learn(total_timesteps=timestep, log_interval=1000)
   
     model.save(filename)
     model.save('./model/'+filename)
